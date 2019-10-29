51c1a55e979818cb52d24104d27904d2caadcdfb
Thomas Dick <thomasjosif@outlook.com>
2019-10-29 02:25:48 -0400

    Remote Controller 02 done. Scary terry logic working & communicating with remote 3 for scary terry lighting.

--- remote-controller02/remote-controller02.ino
+++ remote-controller02/remote-controller02.ino
@@ -21,7 +21,7 @@ byte mac[] = { 0xDC, 0x1A, 0x4D, 0x2B, 0xD5, 0x4D };
 IPAddress ip(192,168,0,22);
 
 char serveraddr = "192.168.1.110";
-byte myserver[] = { 192, 168, 1, 110 }; // zoomkat web page server IP address
+byte myserver[] = { 192, 168, 1, 112 }; // zoomkat web page server IP address
 
 
 // Ethernet comms
@@ -45,24 +45,24 @@ int triggerskull(String command);
 /////////////////
 
 // MOSSFETS
-int MOSSFET_1 = A0; // Yard Light CH1
-int MOSSFET_2 = A1; // Yard Light CH2
-int MOSSFET_3 = A2; // Yard Light CH3
-int MOSSFET_4 = A3; // Yard Light CH4
-int MOSSFET_5 = A4; // Yard Light CH5
+int MOSSFET_1 = 9; // Yard Light CH1
+int MOSSFET_2 = 6; // Yard Light CH2
+int MOSSFET_3 = 5; // Yard Light CH3
+int MOSSFET_4 = 3; // Yard Light CH4
+int MOSSFET_5 = 2; // Yard Light CH5
 
 // RELAYS
-int RELAY_1 = 2; // Skull
-int RELAY_2 = 3; // Spare
-int RELAY_3 = 4; // Spare
-int RELAY_4 = 5; // Spare
+int RELAY_1 = A0; // Skull
+int RELAY_2 = A1; // Spare
+int RELAY_3 = A2; // Spare
+int RELAY_4 = A3; // Spare
 
 // PIRS
-int PIR_1 = A5; //PIR Left Sidewalk
-int PIR_2 = 9; //PIR Right Sidewalk
+int PIR_1 = A4; //PIR Left Sidewalk
+int PIR_2 = A5; //PIR Right Sidewalk
 
 // DFPlayer (MP3 for scary terry)
-int DF_RX = 6; // RX For MP3 Control
+int DF_RX = 4; // RX For MP3 Control
 int DF_TX = 7; // TX For MP3 Control
 int DF_BUSY = 8; // Lets us know if it's still playing a track
 
@@ -75,6 +75,12 @@ DFRobotDFPlayerMini myDFPlayer;
 unsigned long currentMillis = 0;  
 unsigned long previousLedMillis = 0;
 
+unsigned long spookylightingdelay = 0;
+unsigned long terrydelay = 0;
+bool spookylighting = false;
+bool skullactive = false;
+bool terryactive = false;
+bool piractive = true;
 
 // Lightning Variables
 unsigned long timeuntillightning = 0;
@@ -83,11 +89,36 @@ unsigned long timeuntilnextflash = 0;
 int numflashes = 0;
 byte lightningState = LOW;
 
+// Barrel Variables
+unsigned long skulldelay = 0;
+unsigned long activeduration = 0;
+unsigned long timeuntilnextactive = 0;
+int numactive = 0;
+byte skullState = LOW;
+
+
 //void http_communicate_send(String ip, 
 void setup(void)
 {
+  mySoftwareSerial.begin(9600);
   // Start Serial
-  Serial.begin(9600);
+  Serial.begin(115200);
+
+  Serial.println(F("Initializing DFPlayer..."));
+
+  if (!myDFPlayer.begin(mySoftwareSerial)) {  //Use softwareSerial to communicate with mp3.
+    Serial.println(F("Unable to begin. Check connection and SD card, or reset the Arduino."));
+    //while (true);
+  }
+
+  Serial.println(F("DFPlayer Mini online."));
+
+  myDFPlayer.setTimeOut(500);                   // Set serial communictaion time out 500ms
+  myDFPlayer.volume(30);                        // Set volume value (0~30).
+  myDFPlayer.EQ(DFPLAYER_EQ_BASS);              // Set EQ to BASS (normal/pop/rock/jazz/classic/bass)
+  myDFPlayer.outputDevice(DFPLAYER_DEVICE_SD);  // Set device we use SD as default
+  myDFPlayer.enableDAC();                       // Enable On-chip DAC
+  
   // Init variables and expose them to REST API
   pirsidwalkright = 0;
   pirsidewalkleft = 0;
@@ -149,9 +180,12 @@ void loop() {
 
   byte pir1 = digitalRead(PIR_1);
   byte pir2 = digitalRead(PIR_2);
-
-  terry();
-  lightning();
+  //digitalWrite(RELAY_3, !pir1);
+  //digitalWrite(RELAY_4, !pir2);
+  ambientlighting();
+  skulllogic(pir1);
+  terrylogic(pir2);
+  
   // check for serial input
   if (Serial.available() > 0) //if something in serial buffer
   {
@@ -165,9 +199,212 @@ void loop() {
 }
 void sendGET() //client function to send/receive GET request data.
 {
+
+
+}
+
+void ambientlighting() {
+
+  int flashCount = random (15, 45);        // Min. and max. number of flashes each loop
+  int flashBrightnessMin =  25;           // LED flash min. brightness (0-255)
+  int flashBrightnessMax =  200;          // LED flash max. brightness (0-255)
+
+  int flashDurationMin = 1;               // Min. duration of each seperate flash
+  int flashDurationMax = 255;              // Max. duration of each seperate flash
+
+  int nextFlashDelayMin = 1;              // Min, delay between each flash and the next
+  int nextFlashDelayMax = 600;            // Max, delay between each flash and the next
+
+  int lightningdelaymin = 1000;
+  int lightningdelaymax = 5000;
+
+  if(spookylighting)
+  {
+    //Serial.print("Execute lig");
+    if(numflashes <= 0)
+    {
+      //Serial.print("numflash ");
+      numflashes = flashCount;
+    }
+    else
+    {
+      if(lightningState == LOW)
+      {
+          //Serial.print("Lightning low");
+        if(currentMillis >= timeuntilnextflash)
+        {
+            //Serial.print("Lightning ON");
+          int randombright = random(flashBrightnessMin, flashBrightnessMax);
+          analogWrite(MOSSFET_1, randombright);
+          analogWrite(MOSSFET_2, randombright);
+          analogWrite(MOSSFET_3, randombright);
+          analogWrite(MOSSFET_4, randombright);
+          analogWrite(MOSSFET_5, randombright);
+          lightningState = HIGH;
+
+          flashduration = random(flashDurationMin, flashDurationMax) + currentMillis;
+        }
+      }
+      else
+      {
+          //Serial.print("Lightning HIGH");
+        if(currentMillis >= flashduration)
+        {
+            //Serial.print("Lightning OFF");
+          analogWrite(MOSSFET_1, 25);
+          analogWrite(MOSSFET_2, 25);
+          analogWrite(MOSSFET_3, 25);
+          analogWrite(MOSSFET_4, 25);
+          analogWrite(MOSSFET_5, 25);
+          lightningState = LOW;
+          numflashes--;
+          if(numflashes > 0)
+          {
+            timeuntilnextflash = random(nextFlashDelayMin, nextFlashDelayMax) + currentMillis;
+          }
+          else
+          {
+            spookylighting = false;
+            spookylightingdelay = random(6000, 15000) + currentMillis;
+          }
+        }
+      }
+      
+    }
+  }
+  else
+  {
+    analogWrite(MOSSFET_1, 255);
+    analogWrite(MOSSFET_2, 255);
+    analogWrite(MOSSFET_3, 255);
+    analogWrite(MOSSFET_4, 255);
+    analogWrite(MOSSFET_5, 255);
+    if(currentMillis >= spookylightingdelay)
+    {
+          spookylighting = true;
+    }
+  }
+  
+}
+
+void skulllogic(byte pir) {
+  if(skullactive)
+  {
+    int activateCount = random (4, 10);    
+  
+    int activateDurationMin = 1;               
+    int activateDurationMax = 750;           
+  
+    int nextActivateDelayMin = 1;              
+    int nextActivateDelayMax = 500;
+
+    if(numactive <= 0)
+    {
+      //Serial.print("numflash ");
+      numactive = activateCount;
+    }
+    else
+    {
+      if(skullState == LOW)
+      {
+          //Serial.print("Lightning low");
+        if(currentMillis >= timeuntilnextactive)
+        {
+            //Serial.print("Lightning ON");
+          digitalWrite(RELAY_1, HIGH);
+          skullState = HIGH;
+
+          activeduration = random(activateDurationMin, activateDurationMax) + currentMillis;
+        }
+      }
+      else
+      {
+          //Serial.print("Lightning HIGH");
+        if(currentMillis >= activeduration)
+        {
+            //Serial.print("Lightning OFF");
+          digitalWrite(RELAY_1, LOW);
+          skullState = LOW;
+          numactive--;
+          if(numactive > 0)
+          {
+            timeuntilnextactive = random(nextActivateDelayMin, nextActivateDelayMax) + currentMillis;
+          }
+          else
+          {
+            skulldelay = 25000 + currentMillis;
+            skullactive = false;
+          }
+        }
+      }
+      
+    }
+  }
+  else
+  {
+    if(currentMillis >= skulldelay)
+    {
+      if(piractive)
+      {
+        if(pir == HIGH)
+        {
+          skullactive = true;
+        }
+      }
+    }
+  }
+}
+
+
+void terrylogic(byte pir) {
+  if(terryactive)
+  {
+    // If no file is currently being played
+    if(digitalRead(DF_BUSY) == HIGH)
+    {
+      deactivateterry();
+    }
+  }
+  else 
+  {
+    if(currentMillis >= terrydelay)
+    {
+      if(piractive)
+      {
+        if(pir == HIGH)
+        {
+          activateterry();
+        }
+      }
+    }
+  }
+}
+
+void activateterry()
+{
+  terryactive = true;
+  http_terrylight(1);
+  myDFPlayer.playMp3Folder(random(1, 3));
+}
+
+void deactivateterry()
+{
+  terryactive = true;
+  http_terrylight(0);
+  myDFPlayer.pause();
+  terrydelay = random(8000, 10000) + currentMillis;
+}
+
+
+void http_terrylight(int value) {
   if (ethclient.connect(myserver, 80)) {  //starts client connection, checks for connection
     Serial.println("connected");
-    ethclient.println("GET / HTTP/1.0"); //download text
+    if(value) {
+      ethclient.println("POST /triggerterrylight?state=1 HTTP/1.0"); //download text
+    } else {
+      ethclient.println("POST /triggerterrylight?state=0 HTTP/1.0"); //download text
+    }
+    
     ethclient.println(); //end of get request
   }
   else {
@@ -186,23 +423,8 @@ void sendGET() //client function to send/receive GET request data.
   Serial.println("==================");
   Serial.println();
   ethclient.stop(); //stop client
-
 }
-
-void terry() {
-  int delaybetweentalking = 7000;
-
-  // If terry is currently talking
-  if(digitalRead(DF_BUSY) == LOW)
-  {
-    //http_communicate_send(REMOTE_03, "triggerterrylight", 1);
-  }
-  else
-  {
-    //http_communicate_send(REMOTE_03, "triggerterrylight", 0);
-  }
-}
-void lightning() {
+/*void lightning() {
 
   int flashCount = random (3, 15);        // Min. and max. number of flashes each loop
   int flashBrightnessMin =  10;           // LED flash min. brightness (0-255)
@@ -261,34 +483,54 @@ void lightning() {
       
     }
   }
-}
+}*/
 // Disable PIR's for 10s
 int pirdisable(String command) {
 
   // Get state from command
-  //int state = command.toInt();
+  int state = command.toInt();
 
-  //digitalWrite(6,state);
-  //return 1;
+  if(state)
+  {
+    piractive = true;
+  }
+  else
+  {
+    piractive = false;
+  }
+  return 1;
 
 }
 // Trigger Electrical box alarm light
 int triggerterry(String command) {
 
   // Get state from command
-  //int state = command.toInt();
-
-  //digitalWrite(6,state);
-  //return 1;
+  int state = command.toInt();
+  if(state)
+  {
+    activateterry();
+  }
+  else
+  {
+    deactivateterry();
+  }
+  return 1;
 
 }
 // Trigger Pneumatic Air Horn
 int triggerskull(String command) {
-
+  
   // Get state from command
-  //int state = command.toInt();
+  int state = command.toInt();
 
-  //digitalWrite(6,state);
-  //return 1;
+  if(state)
+  {
+    skullactive = true;
+  }
+  else
+  {
+    skullactive = false;
+  }
+  return 1;
 
 }
